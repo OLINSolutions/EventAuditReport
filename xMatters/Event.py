ef43b5a05734dcfe3ca74913090c23e3d6a96c98
Jordan Olin <jordan@olinsolutions.com>
2017-03-07 18:32:30 -0500

    Pylint updates

--- xMatters/Event.py
+++ xMatters/Event.py
@@ -15,22 +15,24 @@ Ref:
 
 import json
 
-from References import PersonReference
+from xMatters.References import PersonReference
+from xMatters.Recipients import Recipient
 
 class Event(object):
     """xMatters Event representation
-    
-    This class represents a view of the xMatters Event that is created as part 
-    of a request for a notification to occur.  The Event class contains both 
-    controlling methods (typically static) for retrieving a list of Event 
+
+    This class represents a view of the xMatters Event that is created as part
+    of a request for a notification to occur.  The Event class contains both
+    controlling methods (typically static) for retrieving a list of Event
     instances, as well as methods for manipulating instances of events.
-    
+
     Attributes:
         See https://help.xmatters.com/xmAPI/?python#event-objects
     """
 
     @classmethod
-    def from_json_obj(cls, json_self:object):
+    def from_json_obj(cls, json_self: object):
+        """Build an Event instance from a JSON object"""
         new_event = cls()
         new_event.id = json_self['id'] if 'id' in json_self else ""
         new_event.event_id = (
@@ -46,26 +48,28 @@ class Event(object):
         new_event.incident = (
             json_self['incident'] if 'incident' in json_self else "")
         new_event.expiration_in_minutes = (
-            json_self['expirationInMinutes'] 
+            json_self['expirationInMinutes']
             if 'expirationInMinutes' in json_self else 0)
         new_event.submitter = (
-            PersonReference.from_json_obj(json_self['submitter']) 
+            PersonReference.from_json_obj(json_self['submitter'])
             if 'submitter' in json_self else PersonReference())
         new_event.recipients = (
-            json_self['recipients'] if 'recipients' in json_self else None)
+            Recipient.from_json_obj(json_self['recipients'])
+            if 'recipients' in json_self else Recipient())
         new_event.form = json_self['form'] if 'form' in json_self else None
         new_event.conference = (
             json_self['conference'] if 'conference' in json_self else None)
         new_event.response_options = (
-            json_self['responseOptions'] 
+            json_self['responseOptions']
             if 'responseOptions' in json_self else None)
         return new_event
-    
+
     @classmethod
-    def from_json_str(cls, json_self:str):
+    def from_json_str(cls, json_self: str):
+        """Build an Event instance from a JSON payload string"""
         obj = json.loads(json_self)
         return cls.from_json_obj(obj)
-    
+
     def __init__(self):
         """Inits Event and defines properties"""
         self.id: str = None
@@ -81,8 +85,9 @@ class Event(object):
         self.form: object = None
         self.conference: object = None
         self.response_options: object = None
-     
+
 def main():
+    """In case we ever need to run as a standalone module"""
     pass
 
 if __name__ == '__main__':
--- xMatters/Recipients.py
+++ xMatters/Recipients.py
@@ -3,28 +3,35 @@
 
 Reference:
     https://help.xmatters.com/xmAPI/#recipient-object
+    https://help.xmatters.com/xmAPI/index.html#dynamic-team-object
     https://help.xmatters.com/xmAPI/#group-object
-
-@author: jolin
 
 .. _Google Python Style Guide:
    http://google.github.io/styleguide/pyguide.html
-
 """
 
 import json
+
 from xMatters.References import SelfLink
 from xMatters.References import ReferenceByIdAndSelfLink
 
 class Recipient(object):
-    
+    """xMatters Recipient representation
+
+    This class represents a view of the xMatters Recipient
+
+    Reference:
+        https://help.xmatters.com/xmAPI/#recipient-object
+    """
+
     @staticmethod
     def _init_from_json_obj(new_obj, json_self: object):
+        """Common initializer converting json object to a Recipient"""
         new_obj.id = json_self['id'] if 'id' in json_self else ""
         new_obj.target_name = (
             json_self['targetName'] if 'targetName' in json_self else "")
         new_obj.recipient_type = (
-            json_self['recipientType'] 
+            json_self['recipientType']
             if 'recipientType' in json_self else "")
         new_obj.external_key = (
             json_self['externalKey'] if 'externalKey' in json_self else "")
@@ -36,18 +43,20 @@ class Recipient(object):
         new_obj.links = (
             SelfLink.from_json_obj(json_self['links'])
             if 'links' in json_self else SelfLink())
-    
+
     @classmethod
     def from_json_obj(cls, json_self: object):
+        """Build an Recipient instance from a JSON object"""
         new_obj = cls()
         cls._init_from_json_obj(new_obj, json_self)
         return new_obj
-    
+
     @classmethod
     def from_json_str(cls, json_self: str):
+        """Build an Recipient instance from a JSON payload string"""
         obj = json.loads(json_self)
         return cls.from_json_obj(obj)
-    
+
     def __init__(self):
         self.id: str = None
         self.target_name: str = None
@@ -57,30 +66,50 @@ class Recipient(object):
         self.locked: [str] = None
         self.status: str = None
         self.links: SelfLink = None
-        
+
 class DynamicTeam(Recipient):
-    
+    """xMatters DynamicTeam representation
+
+    This class represents a view of the xMatters Dynamic Team
+
+    Reference:
+        https://help.xmatters.com/xmAPI/index.html#dynamic-team-object
+    """
+
     @classmethod
     def from_json_obj(cls, json_self: object):
+        """Build a DynamicTeam instance from a JSON object"""
         new_obj = cls()
         Recipient._init_from_json_obj(new_obj, json_self)
         new_obj.recipient_type = "DYNAMIC_TEAM"
         new_obj.status = "ACTIVE"
         new_obj.links = SelfLink()
         return new_obj
-    
+
     @classmethod
     def from_json_str(cls, json_self: str):
+        """Build a DynamicTeam instance from a JSON payload string"""
         obj = json.loads(json_self)
         return cls.from_json_obj(obj)
-    
+
     def __init__(self):
         super().__init__()
-        
+        self.recipient_type: str = None
+        self.status: str = None
+        self.links: SelfLink = None
+
 class Group(Recipient):
-    
+    """xMatters Group representation
+
+    This class represents a view of the xMatters Group
+
+    Reference:
+        https://help.xmatters.com/xmAPI/#group-object
+    """
+
     @classmethod
     def from_json_obj(cls, json_self: object):
+        """Build a Group instance from a JSON object"""
         new_obj = cls()
         Recipient._init_from_json_obj(new_obj, json_self)
         new_obj.allow_duplicates = (
@@ -98,12 +127,13 @@ class Group(Recipient):
             json_self['useDefaultDevices']
             if 'useDefaultDevices' in json_self else False)
         return new_obj
-    
+
     @classmethod
     def from_json_str(cls, json_self: str):
+        """Build a Group instance from a JSON payload string"""
         obj = json.loads(json_self)
         return cls.from_json_obj(obj)
-    
+
     def __init__(self):
         super().__init__()
         self.allow_duplicates: bool = None
@@ -111,8 +141,9 @@ class Group(Recipient):
         self.observed_by_all: bool = None
         self.site: ReferenceByIdAndSelfLink = None
         self.use_default_devices: bool = None
-        
+
 def main():
+    """In case we ever need to run this stand-alone"""
     pass
 
 if __name__ == '__main__':
